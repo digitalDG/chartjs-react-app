d24262398be4b1d71552016870c4e3c1f4f8bea4
David Gilbert <173236+digitalDG@users.noreply.github.com>
2019-12-29 00:16:36 -0500

    Updates

---
+++ precache-manifest.2ef1a4f900540bf11bed716c8f818cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d58ce85f0e8381f399086bb248e1cd3",
+    "url": "/chartjs-react-app/index.html"
+  },
+  {
+    "revision": "47ed98dfc04aca5d9df5",
+    "url": "/chartjs-react-app/static/css/main.eea969ef.chunk.css"
+  },
+  {
+    "revision": "3d7ee2ef8343bf9606cd",
+    "url": "/chartjs-react-app/static/js/2.e994d01e.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/chartjs-react-app/static/js/2.e994d01e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47ed98dfc04aca5d9df5",
+    "url": "/chartjs-react-app/static/js/main.478620bf.chunk.js"
+  },
+  {
+    "revision": "ed55c67acbacccc598ba",
+    "url": "/chartjs-react-app/static/js/runtime-main.6c378efd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chartjs-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
